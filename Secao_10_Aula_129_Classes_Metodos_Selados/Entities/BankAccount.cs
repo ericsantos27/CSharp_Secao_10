9953466fbd1026006d2f4cc2cf4954777e53c9f1
ericsantos27 <esantos_samu@hotmail.com>
2021-01-05 12:07:50 -0300

    Inclusão de ajuste na classe BankAccount

--- Secao_10_Aula_129_Classes_Metodos_Selados/Entities/BankAccount.cs
+++ Secao_10_Aula_129_Classes_Metodos_Selados/Entities/BankAccount.cs
@@ -6,5 +6,26 @@ namespace Secao_10_Aula_129_Classes_Metodos_Selados.Entities
 {
     class BankAccount
     {
+        public int AccountNumber { get; private set; }
+        public string AccountHolder { get; private set; }
+        public double AccountBalance { get; protected set; }
+
+        public BankAccount()
+        {
+        }
+        public BankAccount(int number, string holder, double balance)
+        {
+            AccountNumber = number;
+            AccountHolder = holder;
+            AccountBalance = balance;
+        }
+        public virtual void WithdrawFunds(double amount)
+        {
+            AccountBalance -= (amount + 1.5);
+        }
+        public void DepositFunds(double amount)
+        {
+            AccountBalance += amount;
+        }
     }
 }
